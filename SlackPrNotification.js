c9f1860304092820c1f7283d428812a88e9857c5
masaki.hiramoto <masaki.hiramoto@SG-MAC-053.local>
2020-07-06 22:10:11 +0900

    test isLite

--- SlackPrNotification.js
+++ SlackPrNotification.js
@@ -10,47 +10,64 @@ var authorName = process.env.PULL_REQUEST_AUTHOR_NAME;
 var authorIconUrl = process.env.PULL_REQUEST_AUTHOR_ICON_URL;
 var compareBranchName = process.env.PULL_REQUEST_COMPARE_BRANCH_NAME;
 var baseBranchName = process.env.PULL_REQUEST_BASE_BRANCH_NAME;
+var isLite = process.env.IS_LITE.toLowerCase() === "true";
 var sendHereMention = process.env.IS_SEND_HERE_MENTION.toLowerCase() === "true" ? "<!here>\n" : "";
-var message = {
-    blocks: [
-        {
-            type: "section",
-            text: {
-                type: "mrkdwn",
-                text: sendHereMention + "*<" + prUrl + "|" + prTitle + ">*"
+if (isLite) {
+    var message = {
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: sendHereMention + "*<" + prUrl + "|" + prTitle + ">*"
+                }
             },
-            accessory: {
-                type: "image",
-                image_url: authorIconUrl,
-                alt_text: "github icon"
+        ]
+    };
+    axios_1["default"].post(url, message);
+}
+else {
+    var message = {
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: sendHereMention + "*<" + prUrl + "|" + prTitle + ">*"
+                },
+                accessory: {
+                    type: "image",
+                    image_url: authorIconUrl,
+                    alt_text: "github icon"
+                },
+                fields: [
+                    {
+                        type: "mrkdwn",
+                        text: "*Author*\n" + authorName
+                    },
+                    {
+                        type: "mrkdwn",
+                        text: "*Base branch*\n" + baseBranchName
+                    },
+                    {
+                        type: "mrkdwn",
+                        text: "*Pull request number*\n#" + prNum
+                    },
+                    {
+                        type: "mrkdwn",
+                        text: "*Compare branch*\n" + compareBranchName
+                    },
+                ]
             },
-            fields: [
-                {
-                    type: "mrkdwn",
-                    text: "*Author*\n" + authorName
-                },
-                {
-                    type: "mrkdwn",
-                    text: "*Base branch*\n" + baseBranchName
-                },
-                {
-                    type: "mrkdwn",
-                    text: "*Pull request number*\n#" + prNum
-                },
-                {
-                    type: "mrkdwn",
-                    text: "*Compare branch*\n" + compareBranchName
-                },
-            ]
-        },
-        {
-            type: "section",
-            text: {
-                type: "plain_text",
-                text: prBody,
-                emoji: true
-            }
-        },
-    ]
-};
-axios_1["default"].post(url, message);
+            {
+                type: "section",
+                text: {
+                    type: "plain_text",
+                    text: prBody,
+                    emoji: true
+                }
+            },
+        ]
+    };
+    axios_1["default"].post(url, message);
+}
