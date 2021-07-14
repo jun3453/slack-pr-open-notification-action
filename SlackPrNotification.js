22ad08973ac0b1cb9e8bb4a1b8604525b8ed4a4a
Mahboi <alexandre.weber@epitech.eu>
2021-07-14 15:15:29 +0200

    change of the look of prNum

--- SlackPrNotification.js
+++ SlackPrNotification.js
@@ -25,7 +25,7 @@ if (makePretty){
                     block_id: "commit_title",
                     text: {
                         type: "mrkdwn",
-                        text: "(#" + prNum + ") *" + prTitle + "* requests merge from *" + baseBranchName + "* to *" + compareBranchName + "*." + sendHereMention
+                        text: "[*#" + prNum + "*] *" + prTitle + "* requests merge from *" + baseBranchName + "* to *" + compareBranchName + "*." + sendHereMention
                     }
                 },
                 {
